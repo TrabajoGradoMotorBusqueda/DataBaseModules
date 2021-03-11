056a8f1f4c77660a7d780887f035dbc2068a2a0a
David Pinchao <sangeeky@outlook.com>
2021-03-11 01:24:29 -0500

    Script Limpieza de Datos Y Agregar datos a la BD

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+from math import isnan as nan
 import db
-from models import ResumenesDocente, ResumenesEstudiante
+from models import ResumenesInvestigacion
+from limpieza_datos import resumenes_estudiantes, resumenes_docentes, estructura_dataset
+
+docentes = estructura_dataset(resumenes_docentes)
+estudiantes = estructura_dataset(resumenes_estudiantes)
 
 
 def run():
-    pass
+    datos = docentes.iloc[0]
+    cols = docentes.columns
+    test = {}
+    for i, col in enumerate(cols):
+        try:
+            value = int(datos[i]) if not nan(datos[i]) else None
+        except:
+            value = datos[i]
+
+        test[col] = value
+
+    test_class = ResumenesInvestigacion(test)
+    db.session.add(test_class)
+    db.session.commit()
+    print('Hola')
+
 
 
 if __name__ == '__main__':
